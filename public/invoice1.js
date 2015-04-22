50fa8e200699e06fc14e7a22f46250162e9236c5
Steven Hafen <hafensteven@gmail.com>
2015-04-22 14:57:12 +0000

    moved logic into controller on the invoice1.js file seperating the view from the logic, created to convert currencies and control the view

---
+++ public/invoice1.js
@@ -0,0 +1,28 @@
+(function(angular) {
+    'use strict';
+    angular.module('invoice1', [])
+    .controller('InvoiceController', function() {
+        this.quantity = 1;
+        this.cost = '';
+        this.inCurr = 'USD';
+        this.currencies = ['USD','EUR','CNY'];
+        this.usdToForeignRates = {
+          USD: 1,
+          EUR: 0.74,
+          CNY:6.09
+        };
+        
+        this.total = function total(outCurr) {
+            return this.convertCurrency(this.quantity * this.cost, this.inCurr, outCurr);
+        };
+        
+        this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
+            return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];  
+        };
+        
+        this.pay = function pay() {
+            window.alert("Thanks!");  
+        };
+        
+    });
+})(window.angular);
